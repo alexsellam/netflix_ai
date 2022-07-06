accec20b11c875c27fffcd4bd8b3cf1d2a995f2d
User <alex.sellam@gmail.com>
2022-07-06 18:39:52 +0200

    fetching genres from tmdb api

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -3,7 +3,9 @@ import { Divider, List, ListItem, ListItemText, ListSubheader, ListItemIcon, Box
 import { Link } from 'react-router-dom';
 import { useTheme } from '@mui/styles';
 
+import { useGetGenresQuery } from '../../services/TMDB'
 import useStyles from './styles';
+import genreIcons from '../../assets/genres';
 
 const categories = [
     {label: 'Popular' , value: 'popular' },
@@ -11,12 +13,6 @@ const categories = [
     {label: 'Upcoming' , value: 'upcoming'},
 ]
 
-const demoCategories = [
-    {label: 'Comedy' , value: 'comedy' },
-    {label: 'Action', value: 'action' },
-    {label: 'Horror' , value: 'horror'},
-    {label: 'Animation' , value: 'animation'},
-]
 
 const redLogo = 'https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png';
 const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png';
@@ -24,6 +20,9 @@ const blueLogo = 'https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48
 const Sidebar = ({ setMobileOpen }) => {
     const theme = useTheme();
     const classes = useStyles();
+    const { data, isFetching } = useGetGenresQuery();
+    console.log(data);
+
   return (
     <>  
     <Link to="/" className={classes.imageLink}>
@@ -39,12 +38,12 @@ const Sidebar = ({ setMobileOpen }) => {
         {categories.map(({label, value})=> (
             <Link key={value} className={classes.links} to="/">
                 <ListItem onClick={()=> {}} button>
-                {/* 
+               
                  <ListItemIcon>
-                    <img src={redLogo} className={classes.genreImages} height={30}/>
-                    </ListItemIcon>
-                */}   
-                    <ListItemText primary={label}/>
+                    <img src={genreIcons[label.toLowerCase()]} className={classes.genreImages} height={30}/>
+                </ListItemIcon>
+                
+                <ListItemText primary={label}/>
                   
                 </ListItem>
             </Link>
@@ -54,15 +53,18 @@ const Sidebar = ({ setMobileOpen }) => {
     <Divider />
     <List>
         <ListSubheader> Genres</ListSubheader>
-        {demoCategories.map(({label, value})=> (
-            <Link key={value} className={classes.links} to="/">
+        {isFetching ? (
+            <Box display="flex" justifyContent="center">  
+                <CircularProgress size="4rem"/>
+            </Box>
+            )
+            : data.genres.map(({name, id})=> (
+            <Link key={name} className={classes.links} to="/">
                 <ListItem onClick={()=> {}} button>
-                {/* 
-                 <ListItemIcon>
-                    <img src={redLogo} className={classes.genreImages} height={30}/>
-                    </ListItemIcon>
-                */}   
-                    <ListItemText primary={label}/>
+                <ListItemIcon>
+                    <img src={genreIcons[name.toLowerCase()]} className={classes.genreImages} height={30}/>
+                </ListItemIcon>
+                    <ListItemText primary={name}/>
                   
                 </ListItem>
             </Link>
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -1,3 +1,4 @@
+import { $CombinedState } from '@reduxjs/toolkit';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const tmdbApiKey = process.env.REACT_APP_TMDB_KEY;
@@ -7,6 +8,10 @@ export const tmdbApi = createApi({
     reducerPath: 'tmdbApi',
     baseQuery: fetchBaseQuery({baseUrl: 'https://api.themoviedb.org/3'}),
     endpoints: (builder) =>({
+        //*Get Genres
+        getGenres: builder.query({
+            query: () => `genre/movie/list?api_key=${tmdbApiKey}`
+        }),
         //*Get movies by [type]
         getMovies: builder.query({
             query: () =>  `movie/popular?page=${page}&api_key=${tmdbApiKey}`
@@ -18,4 +23,5 @@ export const tmdbApi = createApi({
 
 export const {
     useGetMoviesQuery,
+    useGetGenresQuery, 
 } = tmdbApi; 
